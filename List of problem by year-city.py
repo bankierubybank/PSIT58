f5473935e6d308333e992c8ebe35917a71921d06
easuck <it58070020@office365.it.kmitl.ac.th>
2015-12-07 20:11:30 +0700

    Draw graph from problem by city

--- List of problem by year-city.py
+++ List of problem by year-city.py
@@ -15,7 +15,7 @@ def list_of_problem_year(year):
 def list_of_problem_city(city):
     """print list of problem by city"""
     import csv
-    years = list(range(2551, 2553))
+    years = list(range(2551, 2555))
     temp = []
     sum_of_problem = 0
     for year in years:
@@ -33,6 +33,28 @@ def list_of_problem_city(city):
         temp.append(data_per_year)
     for i in range(len(temp)):
         sum_of_problem += sum(temp[i][1:])
-        print('Year', str(2551 + i), '=', temp[i])
-    print('Sum of Problem in ' + str(len(years)), 'Years =', sum_of_problem)
-list_of_problem_city('Bangkok')
+    return temp
+
+def test_plot():
+    """Test matplotlib"""
+    import matplotlib.pyplot as plt
+    import numpy as np
+    x = [2551,2552,2553,2554]
+    y = []
+    for i in range(4):
+        temp = list_of_problem_city('Bangkok')[i][1:]
+        y.append(temp)
+        print(temp)
+    y = np.array(y)
+    z = y.reshape(4,4)
+    z = z.transpose()
+    plt.title("Test matplotlib with data from list_of_problem_city")
+    p0 = plt.plot(x,z[0], ':bp')
+    p1 = plt.plot(x,z[1], ':r*')
+    p2 = plt.plot(x,z[2], ':gp')
+    p3 = plt.plot(x,z[3], ':kD')
+    plt.legend((p0[0],p1[0],p2[0],p3[0]),('Problem1','Problem2','Problem3','Problem4'))
+    plt.xlabel("Year")
+    plt.ylabel("Y")
+    plt.show()
+test_plot()
