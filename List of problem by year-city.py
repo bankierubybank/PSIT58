5820ea982ffa30e393e0244e332c8f5ae6d2ee24
easuck <it58070020@office365.it.kmitl.ac.th>
2015-12-07 20:49:26 +0700

    Change test_plot into 2 functions

--- List of problem by year-city.py
+++ List of problem by year-city.py
@@ -1,9 +1,12 @@
 """Print list of problem by year/city"""
-def list_of_problem_year(year):
-    """print list of problem by year"""
-    import csv
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+def sum_of_problem_year(year):
+    """Return sum of problem by year"""
     temp = 0
-    selected_data = 'test_pol_stat' + str(year) + '.csv'
+    selected_data = 'pol_stat' + str(year) + '.csv'
     with open(selected_data) as csvfile:
         data = csv.reader(csvfile)
         for row in data:
@@ -11,15 +14,15 @@ def list_of_problem_year(year):
             for i in range(1, len(row)):
                 temp += int(row[i])
     print('END OF YEAR', str(year), 'SUM OF PROBLEM =', str(temp))
+    return temp
 
 def list_of_problem_city(city):
-    """print list of problem by city"""
-    import csv
+    """Return list of problem by city"""
     years = list(range(2551, 2555))
     temp = []
     sum_of_problem = 0
     for year in years:
-        selected_data = 'test_pol_stat' + str(year) + '.csv'
+        selected_data = 'pol_stat' + str(year) + '.csv'
         with open(selected_data) as csvfile:
             data = csv.reader(csvfile)
             for row in data:
@@ -35,26 +38,30 @@ def list_of_problem_city(city):
         sum_of_problem += sum(temp[i][1:])
     return temp
 
-def test_plot():
-    """Test matplotlib"""
-    import matplotlib.pyplot as plt
-    import numpy as np
-    x = [2551,2552,2553,2554]
-    y = []
-    for i in range(4):
+def problem_per_year_to_problem_type():
+    """Convert problem per year(input from list_of_problem_city)
+    into problem type"""
+    year = 4
+    problem = []
+    for i in range(year):
         temp = list_of_problem_city('Bangkok')[i][1:]
-        y.append(temp)
-        print(temp)
-    y = np.array(y)
-    z = y.reshape(4,4)
-    z = z.transpose()
-    plt.title("Test matplotlib with data from list_of_problem_city")
-    p0 = plt.plot(x,z[0], ':bp')
-    p1 = plt.plot(x,z[1], ':r*')
-    p2 = plt.plot(x,z[2], ':gp')
-    p3 = plt.plot(x,z[3], ':kD')
-    plt.legend((p0[0],p1[0],p2[0],p3[0]),('Problem1','Problem2','Problem3','Problem4'))
+        problem.append(temp)
+    problem = np.array(problem)
+    array = problem.reshape(year,10)
+    array = array.transpose()
+    return array
+    
+def plot_problem_by_city():
+    """Plot problem by city"""
+    years = [2551,2552,2553,2554]
+    array = problem_per_year_to_problem_type()
+    plt.title("Plot problem by city")
+    p1 = plt.plot(years,array[0], ':bp')
+    p2 = plt.plot(years,array[1], ':r*')
+    p3 = plt.plot(years,array[2], ':gp')
+    p4 = plt.plot(years,array[3], ':kD')
+    plt.legend((p1[0],p2[0],p3[0],p4[0]),('Problem1','Problem2','Problem3','Problem4'))
     plt.xlabel("Year")
     plt.ylabel("Y")
     plt.show()
-test_plot()
+plot_problem_by_city()
