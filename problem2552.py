8e43c8708a0993aeab0afd4b1f4cce55571def5a
chychananya <s8070022@kmitl.ac.th>
2015-12-09 14:20:53 +0700

    Update problem2552.py

--- problem2552.py
+++ problem2552.py
@@ -3,7 +3,7 @@ import csv
 file=open('pol_stat2552-Table-1.csv')
 data=csv.reader(file)
 table=[row for row in data]
-province, problem = [], []
+province, problem_1 = [], []
 for i in table[1:78]:
     province.append(i[0])
     problem_1.append(i[1])
