14a672e368d25c360f1de95bcdb45e2a7e8ec1be
chychananya <s8070022@kmitl.ac.th>
2015-12-09 14:14:28 +0700

    Create problem2552.py
    just import data in year 2552 okay,bebe!

---
+++ problem2552.py
@@ -0,0 +1,9 @@
+#import problem year 2552
+import csv
+file=open('pol_stat2552-Table-1.csv')
+data=csv.reader(file)
+table=[row for row in data]
+province, problem = [], []
+for i in table[1:78]:
+    province.append(i[0])
+    problem_1.append(i[1])
